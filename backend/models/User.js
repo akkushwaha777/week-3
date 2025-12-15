14949b05f2264f5bad703c5667e1393fe62c922e
Amit K. Prasad <kus8347052@gmail.com>
2025-12-15 20:55:59 +0530

    Add files via upload

---
+++ backend/models/User.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    role: { type: String, required: true },
+    mobile: { type: String },
+    email: { type: String }
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
